991a0bde22b63002bdc64497abcc18d4e69a8892
unknown <Charlotte Landuyt>
2022-12-19 12:06:22 +0100

    exo4

---
+++ public/js/javascript.js
@@ -0,0 +1,5 @@
+let chose = prompt("insérez une phrase");
+let change = prompt("quel mot faut-il changer?");
+let replace = prompt("quel mot devra le remplacer ?");
+
+alert(chose.replace(change, replace));
